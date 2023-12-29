a7eaaf2e4d5a5a8560ec2083f6e867b53a4cdf36
hosseinhj1380 <hosseinjavaditj@gmail.com>
2023-12-29 21:43:39 +0330

    add sso authentication with google

--- backend/app/core/auth/authentication.py
+++ backend/app/core/auth/authentication.py
@@ -4,11 +4,48 @@ from fastapi.exceptions import HTTPException
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from crud.users_crud import authenticate_user
 from core.hash import Hash
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
+import requests
+from jose import jwt
 
 from . import oauth2
 
 
 router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Replace these with your own values from the Google Developer Console
+GOOGLE_CLIENT_ID = "670828547454-jsui80lil4jb28597qlevcdj9i7oc5ho.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
+GOOGLE_REDIRECT_URI = "http://127.0.0.1:8000/"
+
+@router.get("/login/google")
+def login_google():
+    return {
+        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={GOOGLE_CLIENT_ID}&redirect_uri={GOOGLE_REDIRECT_URI}&scope=openid%20profile%20email&access_type=offline"
+    }
+
+@router.get("/")
+def auth_google(code: str):
+    token_url = "https://accounts.google.com/o/oauth2/token"
+    
+    data = {
+        "code": code,
+        "client_id": GOOGLE_CLIENT_ID,
+        "client_secret": GOOGLE_CLIENT_SECRET,
+        "redirect_uri": GOOGLE_REDIRECT_URI,
+        "grant_type": "authorization_code",
+    }
+    response = requests.post(token_url, data=data)
+    access_token = response.json().get("access_token")
+    user_info = requests.get("https://www.googleapis.com/oauth2/v1/userinfo", headers={"Authorization": f"Bearer {access_token}"})
+    print(user_info.json())
+    return user_info.json()
+
+@router.get("/google/token")
+async def get_token(token: str = Depends(oauth2_scheme)):
+    return jwt.decode(token, GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
 
 
 @router.post("/api/oauth2/login")
