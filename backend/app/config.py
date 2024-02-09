9c72f088d2248ff0e97fc8e06e8932de43ee8a1e
hosseinhj1380 <hosseinjavaditj@gmail.com>
2024-02-09 22:31:59 +0330

    add allow cors origin request on middlewear

--- backend/app/config.py
+++ backend/app/config.py
@@ -2,8 +2,18 @@ from fastapi import FastAPI
 from routers import movies,category,comments,genres,persons,users , cinemas , facilities
 
 from core.auth import authentication 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(movies.router, tags=["movie"])
 app.include_router(genres.router,tags=["genres"])
 app.include_router(category.router,tags=["category"])
